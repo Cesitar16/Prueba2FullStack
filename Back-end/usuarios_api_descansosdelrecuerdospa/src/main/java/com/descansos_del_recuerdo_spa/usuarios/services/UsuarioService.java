240b83d199ec122ef0ae1baa31aaff387ff98b10
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-10-22 21:01:46 -0300

    Agrgué un metodo patch a controller para asi poder modificar usuarios

--- Back-end/usuarios_api_descansosdelrecuerdospa/src/main/java/com/descansos_del_recuerdo_spa/usuarios/services/UsuarioService.java
+++ Back-end/usuarios_api_descansosdelrecuerdospa/src/main/java/com/descansos_del_recuerdo_spa/usuarios/services/UsuarioService.java
@@ -13,4 +13,5 @@ public interface UsuarioService {
     Usuario update(Long id, Usuario u);   // sin cambiar pass
     void changePassword(Long id, String newPassword);
     void delete(Long id);
+    Usuario save(Usuario usuario);
 }
